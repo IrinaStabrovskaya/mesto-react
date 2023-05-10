4847a31b62d15e3c5c31b86095ff7816dc84cc3a
Irina Stabrovskaya <foxy-54@mail.ru>
2023-05-10 08:25:57 +0800

    fix: исправление ошибок после ревью в компоненте Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+
+//в этом компоненте использован дополнительный функционал
+// контейнера лайков и отображения количества лайков
+//т.е. если лайков нет, то их количество не отображается, если есть, то все отображается по ТЗ.
+//Этот функционал был применен в ПР9, поэтому реализован и этой работе.
 
 const Card = ({ link, name, likes, onCardClick }) => {
-  //в этом компоненте использовано 2 useState и 1 useEffect не по ТЗ,
-  //они необходимы для реализации функционала контейнера лайков и отображения количества лайков
-  //т.е. если лайков нет, то их количество не отображается, если есть, то все отображается по ТЗ.
-  //Этот функционал был применен в ПР9, поэтому реализован и этой работе.
-  const [counterContainer, setCounterContainer] = useState(true);
-  const [likesLength, setLikesLength] = useState(true);
-
-  function handleCounterContainer() {
-    setCounterContainer(false);
-  }
-
-  function onVisible() {
-    setLikesLength(false);
-  }
-
-  useEffect(() => {
-    if (likes.length == 0) {
-      onVisible();
-      handleCounterContainer();
-    }
-  });
+  const hasLikes = likes.length !== 0;
 
   function handleClick() {
     onCardClick({ link, name });
@@ -29,7 +14,6 @@ const Card = ({ link, name, likes, onCardClick }) => {
 
   return (
     <li className="photos__element">
-    <>
       <button className="photos__delete-btn btn-hover" type="button"></button>
       <img
         className="photos__image"
@@ -41,7 +25,7 @@ const Card = ({ link, name, likes, onCardClick }) => {
         <h2 className="photos__title">{name}</h2>
         <div
           className={`photos__like ${
-            !counterContainer ? "" : `photos__like_with-counter`
+            !hasLikes ? "" : `photos__like_with-counter`
           }`}
         >
           <button
@@ -49,11 +33,10 @@ const Card = ({ link, name, likes, onCardClick }) => {
             type="button"
           ></button>
           <div className="photos__like-counter">
-            {!likesLength ? "" : `${likes.length}`}
+            {!hasLikes ? "" : `${likes.length}`}
           </div>
         </div>
       </div>
-      </>
     </li>
   );
 };
